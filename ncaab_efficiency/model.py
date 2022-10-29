945f316aec3f8ecb23903c7ede037f2b2b081050
Liam Monninger <l-monninger@Liams-MacBook-Air.local>
2022-10-28 23:24:38 -0700

    Cleaning.

--- ncaab_efficiency/model.py
+++ ncaab_efficiency/model.py
@@ -1,79 +1,57 @@
-from typing import List
+from typing import List, Dict
 from gdg_model_builder import Model, \
     private, session, spiodirect, universal, \
     poll, days, secs, dow, Event
 from pydantic import BaseModel
 
-class LeagueEfficiency(BaseModel):
-    # ppp_o and ppp_d = points scored (for and against)/possessions for each game from SportsData.io
-    # ppp_avg = average of all oe and de from league table
-    # opp_de = season defensive efficiency from league table
+class ProjectionEntry(BaseModel):
+    game_id : int
+    home_team_id : int
+    away_team_id : int
+    home_team_score : float
+    away_team_score : float
+
+class EfficiencyEntry(BaseModel):
+    team_id : int
     ppp_o : int
     ppp_d : int
     ppp_avg : float
     opp_de : float
     opp_oe : float
-    pass
-
-class Team():
-    id : int
-    name : str
-    pass
 
 
 ncaab_efficiency = Model(cron_window=1)
 
-@ncaab_efficiency.get("projection_table", universal, t=LeagueEfficiency)
+@ncaab_efficiency.get("projection_table", universal, t=Dict[str, ProjectionEntry])
 def get_projection_table(context, value):
     # get_league_efficiency
     # proj_tempo = tempo + opp_tempo - tempo_avg
     # projection = proj_tempo*(oe+de-ppp_avg)/100
-    pass
+    return value
 
-# IN CASE WE ENCOUNTER COMPLEXITIES
-@ncaab_efficiency.get("projection_table", universal, private, t=LeagueEfficiency)
-def set_projection_table(context, value):
-    # get_league_efficiency
-    pass
-
-@ncaab_efficiency.task(valid=days(1))
-def iterate_projection_table(context, value):
-    # you will want to call set efficiency from here
-    pass
-
-@ncaab_efficiency.get("league_effiency_table", universal, t=LeagueEfficiency)
+@ncaab_efficiency.get("league_effiency_table", universal, t=Dict[str, EfficiencyEntry])
 def get_league_efficiency(context, value):
-    
-    pass
+    return value
 
-@ncaab_efficiency.set("league_effiency_table", universal, t=LeagueEfficiency)
+@ncaab_efficiency.set("league_effiency_table", universal, private, t=Dict[str, EfficiencyEntry])
 def set_league_efficiency(context, value):
-    pass
+    return value
 
-@ncaab_efficiency.get("manual_league_efficiency_table", universal, t=LeagueEfficiency)
+@ncaab_efficiency.get("manual_league_efficiency_table", universal, t=Dict[str, EfficiencyEntry])
 def set_league_table(context, value):
-    # does this need to be state?
-    pass
+    return value
 
-@ncaab_efficiency.set("manual_league_efficiency_table", universal, t=LeagueEfficiency)
+@ncaab_efficiency.set("manual_league_efficiency_table", universal, t=Dict[str, EfficiencyEntry])
 def set_league_table(context, value):
-    # does this need to be state?
-    pass
+    return value
 
-@ncaab_efficiency.get("game_efficiency_tables", universal, t=LeagueEfficiency)
-# game_oe = ppp_o/(opp_de/ppp_avg)
-# game_de = ppp_d/(opp_oe/ppp_avg)
+@ncaab_efficiency.get("game_efficiency_tables", universal, private, t=Dict[str, Dict[str, EfficiencyEntry]])
 def get_game_efficiency(context, value):
-    pass
+    return value
 
-@ncaab_efficiency.set("game_efficiency_tables", universal, t=LeagueEfficiency)
+@ncaab_efficiency.set("game_efficiency_tables", universal, private t=Dict[str, Dict[str, EfficiencyEntry]])
 def set_game_efficiency(context, value):
-    pass
-
-@ncaab_efficiency.get("top25", universal, t=LeagueEfficiency)
-def get_top_25(context, value):
-    # reference get_league_efficiency
-    pass
+    return value
 
 @ncaab_efficiency.task(valid=days(1))
 def iterate_efficiency(context, value):
