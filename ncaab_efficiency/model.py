198386fa35b68c4771e32cb0cca6b5dbe4ef9152
Liam Monninger <79056955+l-monninger@users.noreply.github.com>
2022-11-26 16:02:13 -0800

    Update model.py
    #7

--- ncaab_efficiency/model.py
+++ ncaab_efficiency/model.py
@@ -377,20 +377,22 @@ def update_team_efficiencies(*,
         and home_possessions > 0 and away_possessions > 0
     ):
         
+        home_factor = 1.014 if game.NeutralVenue else 1.0
+        away_factor = 0.986 if game.NeturalVenue else 1.0
         
         game_eff_table_out[home_team_id][game_id] = GameEfficiencyEntry(
             team_id = home_team_id,
             game_id = game.GameID,
-            game_oe = ((game.HomeTeamScore/(home_possessions))/((1.014 * eff_table[away_team_id].de)/ppp_avg)),
-            game_de = ((game.AwayTeamScore/(away_possessions))/((0.986 * eff_table[away_team_id].oe)/ppp_avg)),
+            game_oe = ((game.HomeTeamScore/(home_possessions))/((home_factor * eff_table[away_team_id].de)/ppp_avg)),
+            game_de = ((game.AwayTeamScore/(away_possessions))/((away_factor * eff_table[away_team_id].oe)/ppp_avg)),
             tempo = home_game_t
         )
         
         game_eff_table_out[away_team_id][game_id] = GameEfficiencyEntry(
             team_id = away_team_id,
             game_id = game.GameID,
-            game_oe = ((game.AwayTeamScore/(away_possessions))/((0.986 * eff_table[home_team_id].de)/ppp_avg)),
-            game_de = ((game.HomeTeamScore/(home_possessions))/((1.014 * eff_table[home_team_id].oe)/ppp_avg)),
+            game_oe = ((game.AwayTeamScore/(away_possessions))/((away_factor * eff_table[home_team_id].de)/ppp_avg)),
+            game_de = ((game.HomeTeamScore/(home_possessions))/((home_factor * eff_table[home_team_id].oe)/ppp_avg)),
             tempo = away_game_t
         )
         
